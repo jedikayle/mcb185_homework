2fed55f0ee5dd1744a6f62645f68deb8d1ff5982
store <jedserna@gmail.com>
2025-02-25 16:18:57 -0800

    edits

---
+++ unit2/montypithon.py
@@ -0,0 +1,19 @@
+# monty pi-thon
+import random 
+x = 0 # set initial value for x and y
+y = 0 
+inside = 0 # set initial value for inside and outside
+outside = 0
+total = 0
+
+while True: # infinite loop
+    total += 1
+    x = random.random() # random between 0 and 1
+    y = random.random() # random y between 0 and 1
+
+    distance = x**2 + y**2 # squared distance from (0,0) d^2 = x^2 + y^2
+
+    if distance <=1: # if distance <1 then point inside circle
+        inside += 1
+        piestimate = 4 * (inside / total)
+    print(piestimate)
